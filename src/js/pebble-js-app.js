193f064bddd6aa5c5c50feb9a2b5a70b6431229b
Chetan Taralekar <chetant@gmail.com>
2014-02-09 17:43:38 -0600

    Fixed weather api change

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -56,9 +56,11 @@ function fetchWeather(location, units)
     var response;
     var req = new XMLHttpRequest();
 
+    weather_api_uri = "http://api.openweathermap.org/data/2.1/forecast/city?q="+location+"&units="+units+"&type=hour";
     req.open('GET', 
-             "http://api.openweathermap.org/data/2.1/forecast/city?q="+location+"&units="+units+"&type=hour", 
+             weather_api_uri, 
              true);
+    console.log(weather_api_uri);
     req.onload = function(e)
     {
         if (req.readyState == 4)
@@ -72,8 +74,8 @@ function fetchWeather(location, units)
                 {
                     var weatherResult = response.list[0];
                     temperature = Math.round(weatherResult.main.temp);
-                    iconStr = weatherResult.weather[0].icon
-                    icon = iconFromWeatherId(weatherResult.weather[0].id, iconStr[iconStr.length-1] == "n");
+                    iconStr = weatherResult.weather.icon
+                    icon = iconFromWeatherId(weatherResult.weather.id, iconStr[iconStr.length-1] == "n");
                     console.log(temperature);
                     console.log(icon);
                     city = response.city.name;
